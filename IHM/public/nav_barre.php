b61a0ee5c16435e995af7f879b1c487715871ee3
nouhailaelmohsine <nouhailaaamohsine@gmail.com>
2025-10-17 12:00:33 +0200

    Ajout des fichiers IHM et interface utilisateur

---
+++ IHM/public/nav_barre.php
@@ -0,0 +1,7 @@
+echo '<nav>
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="IHM/Etudiant/affichage.php">Étudiants</a></li>
+        <li><a href="IHM/Prof/affichage.php">Professeurs</a></li>
+    </ul>
+</nav>' > IHM/public/nav_barre.php
